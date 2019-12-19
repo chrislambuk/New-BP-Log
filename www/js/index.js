2f1b8b0ccc76f7a4d68024ddbabfeeca6cc9b81a
Chris <mail.lamb@gmail.com>
2019-12-19 13:45:24 +0000

    v2.0.4
    Removed confirm. It doesn't work

--- www/js/index.js
+++ www/js/index.js
@@ -80,10 +80,8 @@ UI.prototype.showAlert = function(message, className) {
 // }
 function deleteBook(e) {
 	if (e.classList.contains('delete')) {
-		if(confirm('Are you sure?')){
 		e.parentElement.parentElement.remove();
 		removeTaskFromLocalStorage(e);
-		}
 	}
 }
 
