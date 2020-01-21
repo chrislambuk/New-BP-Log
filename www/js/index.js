bca0ae787d733a180f0f675bf7b1e411a7ba2439
Chris <mail.lamb@gmail.com>
2020-01-21 17:01:44 +0000

    3.2.3

--- www/js/index.js
+++ www/js/index.js
@@ -337,8 +337,8 @@ function onDeviceReady() {
 	admob.setOptions({
 		publisherId: 'ca-app-pub-8816517022745547/1947432293', // Required
 		// interstitialAdId:     "ca-app-pub-XXXXXXXXXXXXXXXX/IIIIIIIIII",  // Optional
-		tappxIdiOS: 'pub-51726-ios-1608', // Optional
-		tappxShare: 0.5 // Optional
+		// tappxIdiOS: 'pub-51726-ios-1608', // Optional
+		// tappxShare: 0.5 // Optional
 	});
 
 	admob.createBannerView();
